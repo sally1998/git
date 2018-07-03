e193add81d3a2d9538bc04ce178ae83ac3acd0c1
Huang <HuangAm@moodys.com>
2018-07-03 10:17:38 +0800

    add test

---
+++ test/java/HelloWorld01Test.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.;
+import org.junit.Test;
+
+public class HelloWorldTest{
+  @Test
+  public void testHelloWorld(){
+    System.out.println();
+    assertEquals(1,0);
+   }
+}
