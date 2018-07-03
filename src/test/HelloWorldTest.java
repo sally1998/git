e5aa50510bab433921afcdcf2eac2185b0852c10
Huang <HuangAm@moodys.com>
2018-07-03 10:42:20 +0800

    test

--- src/test/HelloWorldTest.java
+++ src/test/HelloWorldTest.java
@@ -4,7 +4,10 @@ import org.junit.Test;
 public class HelloWorldTest{
   @Test
   public void testHelloWorld(){
-    System.out.println();
+    assertEquals(1,0);
+   }
+  @Test
+  public void testHelloWorld02(){
     assertEquals(1,0);
    }
 }
